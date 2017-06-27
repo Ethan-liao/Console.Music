6a21000d574933454c56133f51fe0bda6c5f8ddc
Daniel Isham <danielisham1@gmail.com>
2017-06-27 10:50:22 -0500

    prepping for deployment

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,6 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'Heroku_postrgres_db'
+    connection: 'postgres://beebiiawyiwiup:6183c5367551e93c65b4167d6413f471cbd84263e5677e01527ca946cd4254f3@ec2-54-221-220-82.compute-1.amazonaws.com:5432/dc8sbkbuj5f8pe'
   }
 };
--- sample_data/posts.js
+++ sample_data/posts.js
@@ -6,23 +6,23 @@ module.exports =
     artist: 'Artist1',
     language: 'JavaScript',
     comment: 'Sample comment',
-    image_url: 'https://i.scdn.co/image/2ce133525413324d9dcdcf1ce648dded7d9b0285'
+    image_url: 'https://i.scdn.co/image/7e661e443083a9f6ce566bf25d28b437a244e5d6'
   },
   {
     type: 'Track',
     title: 'Title2',
     artist: 'Artist2',
     language: 'Python',
-    comment: 'adsnkjwbekjebw sdfwefew',
-    image_url: 'https://i.scdn.co/image/2ce133525413324d9dcdcf1ce648dded7d9b0285'
+    comment: 'Sample comment',
+    image_url: 'https://i.scdn.co/image/7e661e443083a9f6ce566bf25d28b437a244e5d6'
   },
   {
     type: 'Track',
     title: 'Title3',
     artist: 'Artist3',
     language: 'JavaScript',
-    comment: 'naweieooiwudr seirouew',
-    image_url: 'https://i.scdn.co/image/2ce133525413324d9dcdcf1ce648dded7d9b0285'
+    comment: 'Sample comment',
+    image_url: 'https://i.scdn.co/image/7e661e443083a9f6ce566bf25d28b437a244e5d6'
   },
   {
     type: 'Track',
@@ -30,7 +30,7 @@ module.exports =
     artist: 'Artist4',
     language: 'Java',
     comment: 'Java is great',
-    image_url: 'https://i.scdn.co/image/2ce133525413324d9dcdcf1ce648dded7d9b0285'
+    image_url: 'https://i.scdn.co/image/7e661e443083a9f6ce566bf25d28b437a244e5d6'
   },
   {
     type: 'Track',
@@ -38,7 +38,7 @@ module.exports =
     artist: 'Artist5',
     language: 'Ruby',
     comment: 'Ruby rules',
-    image_url: 'https://i.scdn.co/image/2ce133525413324d9dcdcf1ce648dded7d9b0285'
+    image_url: 'https://i.scdn.co/image/7e661e443083a9f6ce566bf25d28b437a244e5d6'
   },
   {
     type: 'Track',
@@ -46,7 +46,7 @@ module.exports =
     artist: 'Artist6',
     language: 'Swift',
     comment: 'This song is the best',
-    image_url: 'https://i.scdn.co/image/2ce133525413324d9dcdcf1ce648dded7d9b0285'
+    image_url: 'https://i.scdn.co/image/7e661e443083a9f6ce566bf25d28b437a244e5d6'
   },
   {
     type: 'Track',
@@ -54,7 +54,7 @@ module.exports =
     artist: 'Artist7',
     language: 'R',
     comment: 'This song is my favorite',
-    image_url: 'https://i.scdn.co/image/2ce133525413324d9dcdcf1ce648dded7d9b0285'
+    image_url: 'https://i.scdn.co/image/7e661e443083a9f6ce566bf25d28b437a244e5d6'
   },
   {
     type: 'Track',
@@ -62,6 +62,6 @@ module.exports =
     artist: 'Artist8',
     language: 'JavaScript',
     comment: 'best ever',
-    image_url: 'https://i.scdn.co/image/2ce133525413324d9dcdcf1ce648dded7d9b0285'
+    image_url: 'https://i.scdn.co/image/7e661e443083a9f6ce566bf25d28b437a244e5d6'
   }
 ];
